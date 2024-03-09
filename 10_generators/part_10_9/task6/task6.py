9e205a3edeacc623e6c6525d0a05ec6c48641dd1
PavloOps <olga.lavrichenko@mail.ru>
2024-03-09 20:53:12 +0300

    solved itertools part_10_9 task 6

--- 10_generators/part_10_9/task6/task6.py
+++ 10_generators/part_10_9/task6/task6.py
@@ -1,5 +1,7 @@
-from itertools import dropwhile
-
+from itertools import dropwhile, compress, count
 
 def first_largest(iterable, number):
-    pass
+    res = next(dropwhile(lambda x: x[1] < number, enumerate(iterable)), -1)
+    return res if isinstance(res, int) else res[0]
+
+first_largest = lambda it, n: next(compress(count(), (i>n for i in it)), -1)
